e7cef833e704d12b19fbbd44adb718ec73af3ec1
Giulio Fieramosca <giuliofieramosca@gmail.com>
2015-06-26 23:48:34 +0200

    Merging overload senza senso - parte 2

--- SevSeg.cpp
+++ SevSeg.cpp
@@ -35,26 +35,6 @@ SevSeg::SevSeg( byte clock, byte latch, byte data, boolean _commonAnode, byte _n
 
 }
 
-/*** GIULIO ***/
-// Prints a number on the display
-void SevSeg::number(byte number) {
-	if (!shiftRegister) {
-        for (byte c = 0, dataTmp = dataArray[number]; c < 7; c++, dataTmp = dataTmp >> 1) {
-            digitalWrite(pinArray[c], commonAnode ? !(dataTmp & 0x01) : dataTmp & 0x01);
-        }
-        
-    }
-    else {
-        if (numberOfDisplays == 1) {
-            digitalWrite(pinArray[1], LOW);
-            shiftOut(pinArray[2], pinArray[0], MSBFIRST, commonAnode ? ~dataArray[number] : dataArray[number]);  
-            digitalWrite(pinArray[1], HIGH);
-        }
-        else
-            this->number(number);
-    }
-}
-
 void SevSeg::number(int number, const bool zeros, const byte base) {
     // Only shift register is allowed
 	if (!shiftRegister) {
@@ -80,39 +60,7 @@ void SevSeg::number(int number, const bool zeros, const byte base) {
     }
     digitalWrite(pinArray[1], HIGH);
 }
-/*** FINE GIULIO ***/
 
-/* Proposta di accorpamento */
-void SevSeg::number(byte num, byte base)
-{
-	// Stampa pefforza 1 sola cifra.. si potrebbe mettere un controllo: pilotaggio diretto && display >1 incompatibile!!
-	// ..a meno di non avere un arduino mega ma a quel punto se la fanno loro la libreria....
-	if (!shiftRegister) {
-        for (byte c = 0, dataTmp = dataArray[num]; c < 7; c++, dataTmp = dataTmp >> 1) {
-            digitalWrite(pinArray[c], commonAnode ? !(dataTmp & 0x01) : dataTmp & 0x01);
-        }
-    }
-    else
-    {
-		digitalWrite(pinArray[latchPin], LOW);
-		for (byte c = 0; c < numberOfDisplays; c++) {
-			// Prints the unit digit
-			shiftOut(pinArray[2], pinArray[0], MSBFIRST, commonAnode ? ~dataArray[num % base] : dataArray[ num % base]);
-			// Next digit
-			num /= base;
-			// Note: first zero is printed
-			// Clear the unused digits
-			if (num == 0) {
-				for (byte k = c; k < numberOfDisplays; k++) {
-					shiftOut(pinArray[2], pinArray[0], MSBFIRST, commonAnode ? 0xFF : 0x00);  
-				}
-				break;
-			}
-		}
-		digitalWrite(pinArray[latchPin], HIGH);
-	}
-}
-/* Fine proposta di accorpamento */
 
 // Print a Hexadecimal number
 void SevSeg::numberHex(byte hexNum)
